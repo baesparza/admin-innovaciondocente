83160d1565d202abad5411e88008a14f5b80a94e
baesparza <bruno.be81@gmail.com>
2018-09-11 20:40:12 -0500

    feat(pwa): add module, icons, and sww

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
 
 @Component({
   selector: 'id-root',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'admin-innovaciondocente';
+
+  constructor(updates: SwUpdate) {
+    updates.available.subscribe(event => {
+      updates.activateUpdate().then(() => document.location.reload());
+    });
+  }
 }
