468590735104438af52ad10e991a6fc5a3517df8
Bruno Esparza <bruno.be81@gmail.com>
2018-09-20 09:52:13 -0500

    feat(banner): add banner and page

---
+++ src/app/modules/programa-formacion/components/banner-cursos/banner-cursos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'id-banner-cursos',
+  templateUrl: './banner-cursos.component.html',
+  styleUrls: ['./banner-cursos.component.scss']
+})
+export class BannerCursosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
