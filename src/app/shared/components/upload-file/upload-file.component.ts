2d07fb5128582adb7884a6a723b8a8f02a03a631
baesparza <bruno.be81@gmail.com>
2018-09-22 16:56:23 -0500

    feat(*): add controls

--- src/app/shared/components/upload-file/upload-file.component.ts
+++ src/app/shared/components/upload-file/upload-file.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { MatDialog } from '@angular/material';
 import { DropZoneComponent } from '../drop-zone/drop-zone.component';
+import { ControlValueAccessor, FormGroup, FormGroupName, FormControlName } from '@angular/forms';
 
 @Component({
   selector: 'id-upload-file',
@@ -9,9 +10,10 @@ import { DropZoneComponent } from '../drop-zone/drop-zone.component';
 export class UploadFileComponent {
 
   @Input() path: string;
-  @Input() type: string = 'imagen';
-
-  url: string = '';
+  @Input() type: string;
+  @Input() label: string;
+  @Input() required: boolean = false;
+  @Input() parentForm: FormGroup;
 
   constructor(public dialog: MatDialog) { }
 
@@ -26,8 +28,9 @@ export class UploadFileComponent {
 
     dialogRef.afterClosed().subscribe(result => {
       console.log('The dialog was closed');
-      this.url = result;
-      console.log(this.url);
+      if (result)
+        this.parentForm.controls['url'].setValue(result);
+      console.log(this.parentForm.value);
     });
   }
 
