b592792776e0b3ba6498818d41580f6c58bc9b34
baesparza <bruno.be81@gmail.com>
2018-09-25 10:22:59 -0500

    feat(create-tip): add form

---
+++ src/app/modules/observatorio/edutendencias/interfaces/tip.ts
@@ -0,0 +1,7 @@
+export interface Tip {
+  name: string,
+  img: string,
+  description: string,
+  link: string,
+  tag: string,
+}
--- src/app/modules/observatorio/edutendencias/pages/create-tip/create-tip.component.ts
+++ src/app/modules/observatorio/edutendencias/pages/create-tip/create-tip.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material';
+import { ActivatedRoute, } from '@angular/router';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'id-create-tip',
@@ -7,9 +11,62 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateTipComponent implements OnInit {
 
-  constructor() { }
+  public tipFormGroup: FormGroup;
+  private shouldUpdate: boolean = false;
+  private tipId: string = null;
+
+  constructor(
+    private _fb: FormBuilder,
+    private _snackBar: MatSnackBar,
+    private _location: Location,
+    private _route: ActivatedRoute,
+    // private service
+  ) { }
 
   ngOnInit() {
+    // get url queries
+    this.tipId = this._route.snapshot.queryParams['id'];
+
+    this.buildForm();
+  }
+
+
+  private buildForm(): void {
+    this.tipFormGroup = this._fb.group({
+      name: ['', Validators.required],
+      img: ['', Validators.required],
+      description: ['', [Validators.required, Validators.minLength(15)]],
+      link: ['', Validators.required],
+      tag: ['', Validators.required],
+    });
+  }
+
+  public submit() {
+    console.log(this.tipFormGroup.value)
+    if (this.tipFormGroup.invalid) {
+      this._snackBar.open('La forma es invalida', null, { duration: 5000, })
+      return;
+    }
+  }
+
+
+  /**
+  * show snack error message
+  * @param e error
+  */
+  private showErrorMessage(e) {
+    this._snackBar.open('Ocurrido un error al guardar, por favor vuelve a intentarlo', null, { duration: 5000, });
+  }
+
+  /////////getters/////////////
+  get name() { return this.tipFormGroup.get('name'); }
+  get img() { return this.tipFormGroup.get('img'); }
+  get description() { return this.tipFormGroup.get('description'); }
+  get link() { return this.tipFormGroup.get('link'); }
+  get tag() { return this.tipFormGroup.get('tag'); }
+
+  get uploadPath() {
+    return `observatorio/${this.name.value}`;
   }
 
 }
