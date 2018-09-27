b36b3e9972287cb14922b2d97759475da4d21d4c
baesparza <bruno.be81@gmail.com>
2018-09-27 10:49:34 -0500

    chore: rename routing modules

--- src/app/layouts/default/default.module.ts
+++ src/app/layouts/default/default.module.ts
@@ -3,12 +3,12 @@ import { CommonModule } from '@angular/common';
 import { LayoutModule } from '@angular/cdk/layout';
 import { MatSidenavModule, MatBottomSheetModule } from '@angular/material';
 
-import { DefaultRoutingModule } from './default.routing.module';
 import { SharedModule } from 'src/app/shared/shared.module';
 
 import { DefaultComponent } from './default.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { MenuComponent } from './components/menu/menu.component';
+import { DefaultRoutingModule } from './default.routing';
 
 @NgModule({
   imports: [
