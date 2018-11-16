478a5681b234708a65f810183c36733288f6ec1d
utpl <bruno.be81@gmail.com>
2018-11-16 10:29:35 -0500

    feat(db): add firestore crud

--- src/app/modules/observatorio/noticias/noticias.module.ts
+++ src/app/modules/observatorio/noticias/noticias.module.ts
@@ -4,12 +4,16 @@ import { NoticiasComponent } from './pages/noticias/noticias.component';
 import { CreateNoticiasComponent } from './pages/create-noticias/create-noticias.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { NoticiasRoutingModule } from './nooticias.routing';
+import { NoticiasService } from './noticias.service';
 
 @NgModule({
   imports: [
     CommonModule,
     SharedModule,
     NoticiasRoutingModule,
+  ],
+  providers:[
+    NoticiasService
   ],
   declarations: [NoticiasComponent, CreateNoticiasComponent]
 })
